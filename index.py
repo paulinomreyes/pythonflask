e8f4458b665b0387568ba649aa95a436c4da359f
paulinomreyes <paulinomreyes@gmail.com>
2017-04-13 12:26:51 +0800

    Update index.py
    Commiting changes

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
-import os
-print "HEllo World"
+from flask import Flask
+application = Flask(__name__)
 
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    application.run()
